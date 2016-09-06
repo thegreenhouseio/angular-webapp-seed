470fc7725ef16936980cff1bae92ba4767c9bec2
Owen Buckley <owen@analogstudios.net>
2016-09-05 22:23:47 -0400

    updated .gitingore and added test

---
+++ src/services/posts.service.spec.ts
@@ -0,0 +1,27 @@
+import { Http } from '@angular/http';
+import { PostInterface, PostsService } from './posts.service';
+
+describe('First Test Suite', () => {
+
+  it('should test PostService', () => {
+    let post: PostInterface = {
+      id: 1,
+      title: 'Post 1 Title',
+      summary: 'Post 1 Summary',
+      createdTime: 1472091258
+    };
+
+    expect(post.title).toEqual('Post 1 Title');
+  });
+
+  it('should test PostService toBeDefined', () => {
+    expect(PostsService).toBeDefined();
+  });
+
+  it('should test PostService get', () => {
+    let posts: Array<PostInterface> = new PostsService(Http).getPosts();
+
+    expect(PostsService).toBeDefined();
+  });
+
+});
