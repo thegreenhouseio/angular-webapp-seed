e7835851f381438b1398cc271f9239ec1fa86ec9
Owen Buckley <owen@analogstudios.net>
2016-09-11 10:21:40 -0400

    RC6 upgrade

--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,7 @@ import { PostDetailsViewComponent } from './views/posts/post-details.component';
 import { Routes } from '@angular/router';
 
 export const APP_ROUTES: Routes = [
-  { path: '', component: <any>HomeViewComponent, terminal: true },
+  { path: '', component: <any>HomeViewComponent },
   { path: 'posts', component: <any> PostsViewComponent },
   { path: 'posts/:id', component: <any> PostDetailsViewComponent },
   { path: 'home', component: <any> HomeViewComponent }
