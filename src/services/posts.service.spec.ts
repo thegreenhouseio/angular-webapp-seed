d1957e0b435fcbcc17c5ba0546c6f2b066571568
Owen Buckley <owen@analogstudios.net>
2016-12-09 08:21:48 -0800

    refactor getPosts

--- src/services/posts.service.spec.ts
+++ src/services/posts.service.spec.ts
@@ -2,7 +2,7 @@ import { PostInterface, PostsService } from './posts.service';
 
 describe('Posts Service Test Suite', () => {
 
-  it('should test PostInterface', () => {
+  it('should exercise PostInterface', () => {
     let post: PostInterface = {
       id: 1,
       title: 'Post 1 Title',
@@ -26,10 +26,14 @@ describe('Posts Service Test Suite', () => {
     expect(posts.length).toEqual(3);
   });
 
-  it('should test PostService.getPosts by id that exists', () => {
-    let posts: Array<PostInterface> = new PostsService().getPosts(1);
+  it('should test PostService.getPosts by an id that exists', () => {
+    let posts: Array<PostInterface> = new PostsService().getPosts(2);
 
     expect(posts.length).toEqual(1);
+    expect(posts[0].id).toEqual(2);
+    expect(posts[0].title).toEqual('Post 2 Title');
+    expect(posts[0].summary).toEqual('Post 2 Summary');
+    expect(posts[0].createdTime).toEqual(1471989627);
   });
 
   it('should test PostService.getPosts by id that doesn not exist', () => {
