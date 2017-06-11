a68b7f3aa094cae922ecf6d97ab16319078fb7e9
Owen Buckley <obuckley@kenzan.com>
2017-06-11 17:16:14 -0400

    cache verify

--- bin/build.sh
+++ bin/build.sh
@@ -11,8 +11,9 @@ npm --version
 echo "clear existing dependencies..."
 rm -rf node_modules/ > /dev/null 2>&1
 
-echo "clearing NPM caches..."
-npm cache clean
+echo "check NPM caches..."
+npm cache verify
+#npm cache clean
 
 echo "installing project dependencie"
 npm install
